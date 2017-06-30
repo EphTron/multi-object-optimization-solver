fa1e204d876b0632e604b8070db4b1a645069efc
sebastianWV <stickert@worldviz.com>
2017-06-30 18:05:53 +0200

    added first naive evolutionary algorithm

--- candidate_solution.py
+++ candidate_solution.py
@@ -14,16 +14,58 @@ def generate_random(feature_dict={}, ensure_valid=True):
         candidate = CandidateSolution(f_dict)
         if ensure_valid and not candidate.is_valid():
             candidate = None
-    return CandidateSolution(f_dict)
+    return candidate
 
+def assess_fitness(candidate, interactions=None):
+    features = candidate.get_feature_list()
+    feature_fitness = 0
+    for feature in features:
+        feature_fitness += feature.value
+
+    interaction_fitness = 0
+    if interactions != None:
+        for i in interactions:
+            interaction_fitness += i.get_value(features)
+
+    return feature_fitness + interaction_fitness
+    
+def arbitrary_crossover(p1, p2, ensure_valid=True):
+    c1 = None
+    c2 = None
+    while c1 is None or c2 is None:
+        c1_features = p1.get_feature_dict()
+        c2_features = p2.get_feature_dict()
+        for f_name in c1_features.keys():
+            if random.randint(0,1) == 1:
+                temp = c1_features[f_name]
+                c1_features[f_name] = c2_features[f_name]
+                c2_features[f_name] = temp
+        c1 = CandidateSolution(c1_features)
+        c2 = CandidateSolution(c2_features)
+        if ensure_valid and (not c1.is_valid() or not c2.is_valid()):
+            c1 = None
+            c2 = None
+    return c1, c2
+            
 class CandidateSolution:
     ''' Contains a configuration of features in a dict.
     Unset features have a None value for specific key. '''
     
+    interactions = None
+    
     def __init__(self, features={}):
         self._features = features
         self._feature_list = [f for f in self._features.values() if f is not None]
+        self._fitness = None
+        if CandidateSolution.interactions != None:
+            self.calc_fitness()
+
+    def calc_fitness(self):
+        self._fitness = assess_fitness(self, CandidateSolution.interactions)
     
+    def get_fitness(self):
+        return self._fitness
+        
     def is_valid(self):
         ''' checks constraints in feature list.
         Returns True if all constraints met. '''
@@ -40,3 +82,8 @@ class CandidateSolution:
         
     def get_feature_dict(self):            
         return self._features
+        
+    def copy_from(self, c):
+        ''' deep copy of candidate_solution data. '''
+        self._features = {f_name:c._features[f_name] for f_name in c._features}
+        self._feature_list = [f for f in self._features.values() if f is not None]
--- main.py
+++ main.py
@@ -9,24 +9,33 @@ import feature_parser
 import candidate_solution
 from candidate_solution import CandidateSolution
 
-def assess_fitness(candidate, interactions):
-    features = candidate.get_feature_list()
-    feature_fitness = 0
-    for feature in features:
-        feature_fitness += feature.value
-
-    interaction_fitness = 0
-    for i in interactions:
-        interaction_fitness += i.get_value(features)
-
-    return feature_fitness + interaction_fitness
-
-def evolution(file_name, verbose):
-    features, interactions = feature_parser.parse(file_name, verbose=verbose)
-    C = [candidate_solution.generate_random(features) for i in range(0,50)]
-    for c in C:
-        print ("fitness:", assess_fitness(c, interactions))
+def evolution(file_name, verbose, generations=50):
+    features, CandidateSolution.interactions = feature_parser.parse(file_name, verbose=verbose)
+    P = [candidate_solution.generate_random(features) for i in range(0,50)]
+    best = None
+    for gen_idx in range(0,generations):
+        for p in P:
+            if best is None or p.get_fitness() > best.get_fitness():
+                best = p
+        Q = []
+        i = 0
+        while i < len(P)-1:
+            p1 = P[i]
+            p2 = P[i+1]
+            c1, c2 = candidate_solution.arbitrary_crossover(p1, p2)
+            Q.append(c1)
+            Q.append(c2)
+            i = i+2
+        P = [q for q in Q]
+        if verbose:
+            print("===== GENERATION ",gen_idx," =====")
+            print("BEST: ", best)
+            print(" > fitness", best.get_fitness())
     
-
+    return best
+    
 if __name__ == "__main__":
-    evolution('src/project_public_1/bdbc', verbose=False)
+    best = evolution('src/project_public_1/bdbc', verbose=True, generations=1000)
+    print("============================= DONE! =============================")
+    print("Best feature list:", best.get_feature_list())
+    

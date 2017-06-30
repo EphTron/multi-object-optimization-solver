853b4eb2e16f64b93665d4a7cf9c45a7af436364
EphTron <es.90@gmx.de>
2017-06-30 15:33:29 +0200

    added fitness assess func

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ Created on Jun 21.06.17 14:30
 @author: ephtron
 """
 
-def assess_fitness(feature_list):
+import feature_parser
+
+
+def assess_fitness(features, interactions):
     feature_fitness = 0
-    for feature in feature_list:
+    for feature in features:
         feature_fitness += feature.value
 
     interaction_fitness = 0
-    # TODO: add
+    for i in interactions:
+        interaction_fitness += i.get_value(features)
 
-    return
-
-
-
-import feature_parser
+    return feature_fitness + interaction_fitness
 
 
 if __name__ == "__main__":

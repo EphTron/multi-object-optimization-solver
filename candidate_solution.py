d0f33ff1153a980884a7c26baaac721331a69f14
sebastianWV <stickert@worldviz.com>
2017-06-30 17:00:07 +0200

    added CandidateSolution and random generation

---
+++ candidate_solution.py
@@ -0,0 +1,42 @@
+﻿import random
+
+def generate_random(feature_dict={}, ensure_valid=True):
+    ''' Brute force generation of valid candidate solution.
+    feature_dict contains ALL possible features. '''
+    candidate = None
+    while candidate == None:
+        f_dict = {}
+        for key in feature_dict:
+            if random.randint(0,1) == 1:
+                f_dict[key] = feature_dict[key]
+            else:
+                f_dict[key] = None
+        candidate = CandidateSolution(f_dict)
+        if ensure_valid and not candidate.is_valid():
+            candidate = None
+    return CandidateSolution(f_dict)
+
+class CandidateSolution:
+    ''' Contains a configuration of features in a dict.
+    Unset features have a None value for specific key. '''
+    
+    def __init__(self, features={}):
+        self._features = features
+        self._feature_list = [f for f in self._features.values() if f is not None]
+    
+    def is_valid(self):
+        ''' checks constraints in feature list.
+        Returns True if all constraints met. '''
+        for feature in self._features.values():
+            if feature == None:
+                continue
+            for ex_feature in feature.exclude_features:
+                if ex_feature in self._features.values():
+                    return False
+        return True
+    
+    def get_feature_list(self):            
+        return self._feature_list
+        
+    def get_feature_dict(self):            
+        return self._features
--- main.py
+++ main.py
@@ -6,9 +6,11 @@ Created on Jun 21.06.17 14:30
 """
 
 import feature_parser
+import candidate_solution
+from candidate_solution import CandidateSolution
 
-
-def assess_fitness(features, interactions):
+def assess_fitness(candidate, interactions):
+    features = candidate.get_feature_list()
     feature_fitness = 0
     for feature in features:
         feature_fitness += feature.value
@@ -19,6 +21,12 @@ def assess_fitness(features, interactions):
 
     return feature_fitness + interaction_fitness
 
+def evolution(file_name, verbose):
+    features, interactions = feature_parser.parse(file_name, verbose=verbose)
+    C = [candidate_solution.generate_random(features) for i in range(0,50)]
+    for c in C:
+        print ("fitness:", assess_fitness(c, interactions))
+    
 
 if __name__ == "__main__":
-    features, interactions = feature_parser.parse('src/project_public_1/bdbc', verbose=False)
+    evolution('src/project_public_1/bdbc', verbose=False)
